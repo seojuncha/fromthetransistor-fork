681ab279450ea22ae36d481bd73f2b131e41c188
seojun cha <sjcha36@gmail.com>
2024-12-19 14:34:05 +0900

    re-implement uart rx

--- mainproject/uart-controller/testbench.py
+++ mainproject/uart-controller/testbench.py
@@ -1,8 +1,38 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, Timer
+from cocotb.triggers import RisingEdge, FallingEdge, Timer
 
 import binascii
+
+"""
+  baudrate ?  bits per sec
+   9600bits/sec
+ clock = f, baudrate = b
+ baudrate period (BT)
+  1 bit = 1 / 9600 sec = 0.0001041666 sec = 0.1041666 ms = 104.166 us = 104166 ns
+  T_baud = 1 / BAUDRATE
+ clock period (t)
+  Clock Frequency (How many clocks in one second)
+  Frequency (F) = the number of clocks(N) / one sec
+  one clock period (T_clk)
+  one sec = T_clk * N
+  T_clk = one sec / N
+  so that T_clk = 1 / F
+  How many clocks are needed to transmit 1 bit?
+ How many clocks = M
+ one clock period = P = 1 / F
+ M * P = total clock count in one second.
+ Time to transmit(or receive) one bit = 1 / B(audrate)
+ So, M * P(sec) = (1 / B)(sec)
+  M = (1 / B) / P = (1 / B) / (1 / F)
+ increate 'count' to generate when the clock cycle reaches to target clock cycle.
+ for instance, 100 MHz, 9600 Baudrate
+ Rquired clcok cycles = (1/9600) / (1/100_000_000) = 10416
+ 0 <= M <= 16384(2**14)
+
+ data frame: 10-bit
+   star bit(1), data bit(8), stop bit(1)
+"""
 
 def char_to_bin(c):
   return format(ord(c), '08b')
@@ -17,16 +47,23 @@ async def tb_uart(dut):
 
   BAUDRATE = 115200
   onebit_ns = int((1/BAUDRATE) * 1000 * 1000 * 1000)
-  baud_tick = int((1/BAUDRATE) / (1/(c.frequency*1_000_000)))
-  dut.baud_tick_max.value = baud_tick
+  clock_div = int((c.frequency*1_000_000) / (BAUDRATE * 16))
+  dut._log.info("clock div: %d", clock_div)
+  dut.baud_tick_max.value = clock_div
 
+  dut.rst.value = 0
+  await Timer(10, units="ns")
+  dut.rst.value = 1
+ 
   while True:
     data = input("Enter the string(exit): ")
     if data == "exit":
       break
     print(f"input : {data}")
+
     for c in data:
       binchar = char_to_bin(c)
+      dut._log.info("binchar: %s", binchar)
       for i in range(10):
         if i == 0:
           in_val = 0
@@ -35,9 +72,11 @@ async def tb_uart(dut):
         else:
           in_val = binchar[i-1]
         dut.rx.value = int(in_val)
-        await Timer(onebit_ns, units="ns")
-        dut._log.info("e %d / send %d rx %s / i %d / reg %s", dut.enable, int(in_val), dut.rx.value, dut.shift_index.value, dut.shift_reg.value.binstr)
-
+        for i in range(clock_div):
+          await RisingEdge(dut.clk)
+        dut._log.info("in: %d rx: %s b: %s shift_reg: %s", int(in_val), dut.rx.value, dut.bit_count.value, dut.shift_reg.value.binstr)
       await RisingEdge(dut.clk)
-      char = binascii.unhexlify("%x" % int("0b"+dut.shift_reg.value.binstr[::-1][1:9], 2))
-      dut._log.info("final result: %s [%s] ", char, dut.shift_reg.value.binstr)
+      dut._log.info("in: %d rx: %s b: %s data_out: %s", int(in_val), dut.rx.value, dut.bit_count.value, dut.rx_data_out.value.binstr)
+
+      char = binascii.unhexlify("%x" % int("0b"+dut.rx_data_out.value.binstr[::-1], 2))
+      dut._log.info("final result: %s [%s] ", char, dut.rx_data_out.value.binstr)
--- mainproject/uart-controller/uart.v
+++ mainproject/uart-controller/uart.v
@@ -1,76 +1,92 @@
 module uart (
   input clk,
-  input reset,
-  input rx,
-  output reg enable,   // 0: disabled, 1: enabled
+  input rst,
+  input rx,    // external
+  output reg [7:0] rx_data_out,  // rx -> sfhit_reg -> data_out
+
+  input tx_start,
+  output tx,   // external
+  output tx_ready,
+  input [7:0] tx_data_in,  // data_in -> shift_reg -> tx
 
   input [13:0] baud_tick_max
 ); 
-//  baudrate ?  bits per sec
-//   9600bits/sec
-// clock = f, baudrate = b
-// baudrate period (BT)
-//  1 bit = 1 / 9600 sec = 0.0001041666 sec = 0.1041666 ms = 104.166 us = 104166 ns
-//  T_baud = 1 / BAUDRATE
+  localparam IDLE = 2'b00,
+            TX_DATA = 2'b01,
+            RX_DATA = 2'b10;
 
-// clock period (t)
-//  Clock Frequency (How many clocks in one second)
-//  Frequency (F) = the number of clocks(N) / one sec
-//  one clock period (T_clk)
-//  one sec = T_clk * N
-//  T_clk = one sec / N
-//  so that T_clk = 1 / F
+  reg [1:0] state, next_state;
 
-//  How many clocks are needed to transmit 1 bit?
-// How many clocks = M
-// one clock period = P = 1 / F
-// M * P = total clock count in one second.
-// Time to transmit(or receive) one bit = 1 / B(audrate)
-// So, M * P(sec) = (1 / B)(sec)
-//  M = (1 / B) / P = (1 / B) / (1 / F)
-// increate 'count' to generate when the clock cycle reaches to target clock cycle.
-// for instance, 100 MHz, 9600 Baudrate
-// Rquired clcok cycles = (1/9600) / (1/100_000_000) = 10416
-// 0 <= M <= 16384(2**14)
   reg [13:0] baud_tick_counter;
 
-  reg [3:0] shift_index;
-  /* verilator lint_off UNUSEDSIGNAL */
-  reg [9:0] shift_reg;
+  reg [3:0] bit_count;
+  reg [7:0] shift_reg;
 
-// data frame: 10-bit
-//   star bit(1), data bit(8), stop bit(1)
+  assign tx = shift_reg[7];
 
-  initial begin
-    baud_tick_counter = 0;
-    enable = 1;
-    shift_reg = 0;
-    shift_index = 0;
+  always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+      baud_tick_counter <= 0;
+      rx_data_out <= 0;
+      bit_count <= 0;
+      shift_reg <= 0;
+    end else begin
+      case(state)
+        IDLE: begin
+          bit_count <= 0;
+          shift_reg <= 0;
+          baud_tick_counter <= 0;
+        end
+        TX_DATA: begin
+          
+        end
+        RX_DATA: begin
+          baud_tick_counter <= baud_tick_counter + 1;
+          if (baud_tick_counter == baud_tick_max) begin
+            $display("bitcount: %d rx : %d",bit_count, rx);
+            bit_count <= bit_count + 1;
+            shift_reg <= {rx, shift_reg[7:1]};
+            baud_tick_counter <= 0;
+            if (bit_count == 8) begin
+              $display("match!");
+              rx_data_out <= shift_reg;
+            end
+          end
+        end
+      endcase
+    end
   end
 
-  always @(posedge clk or negedge reset) begin
-    if (reset) begin
-      baud_tick_counter <= 0;
-      enable <= 1;
-      shift_reg <= 0;
-    end else if (enable && !rx) begin    // start receiving
-      enable <= 0;
-      shift_index <= shift_index + 1;
-      shift_reg[shift_index] <= rx;
-    end else if (!enable) begin          // keep receiving
-      baud_tick_counter <= baud_tick_counter + 1;
-      if (baud_tick_counter >= baud_tick_max) begin
-        baud_tick_counter <= 0;
-        if (shift_index == 9) begin
-          shift_reg[shift_index] <= rx;
-          shift_index <= 0;
-          enable <= 1;
-        end else begin
-          shift_index <= shift_index + 1;
-          shift_reg[shift_index] <= rx;
+  // state control
+  always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+      state <= IDLE;
+    end else begin
+      state <= next_state;
+      
+    end
+  end
+
+  always @(*) begin
+    next_state = state;
+    case(state)
+      IDLE: begin
+        if(tx_start)
+          next_state = TX_DATA;
+        if (rx == 0) begin
+          next_state = RX_DATA;
         end
       end
-    end
+      TX_DATA: begin
+
+      end
+      RX_DATA: begin
+        if (bit_count > 8) begin
+          $display("DONE");
+          next_state = IDLE;
+        end
+      end
+    endcase
   end
 
 endmodule
