16baf57d0e057ceab3b0f1ae509afef39e93fc02
seojun cha <sjcha36@gmail.com>
2024-12-24 14:45:32 +0900

    make classes for assembler

--- mainproject/assembler/as.py
+++ mainproject/assembler/as.py
@@ -5,23 +5,7 @@ import os
 from elftools.elf.elffile import ELFFile
 from capstone import Cs, CS_ARCH_ARM, CS_MODE_ARM
 
-"""
-ver 1:
- do
-- only encode a instruction line
- test
-- compare the output of own as with the original's
-
-Contents of the line
-- directives
-- instructions
-  - start with indentation
-- labels
-- comments
-- data directives
-"""
-
-cond_map = {
+condition_code_map = {
   "eq": 0b0000,
   "ne": 0b0001,
   "cs": 0b0010,
@@ -40,7 +24,7 @@ cond_map = {
   "nv": 0b1111,
 }
 
-opcode_map = {
+data_opcode_map = {
   "and": 0b0000,
   "eor": 0b0001,
   "sub": 0b0010,
@@ -59,7 +43,7 @@ opcode_map = {
   "mvn": 0b1111,
 }
 
-shift_map = {
+shift_code_map = {
   "lsl": 0b00, # Logical Shift Left
   "lsr": 0b01, # Logical Shift Right
   "asr": 0b10, # Arithmetic Shift Right
@@ -67,82 +51,193 @@ shift_map = {
 }
 # "rrx"  # Rotate Right with Extend
 
-def bits(v, p):
-  return (v << p)
+data_processing_type1_list = ["mov", "mvn"]
+data_processing_type2_list = ["cmp", "cmn", "tst", "teq"]
+data_processing_type3_list = ["add", "sub", "rsb", "adc", "sbc", "rsc", "and", "bic", "eor", "orr"]
 
-def process_instruction():
-  # remove comments
-  inst_line = line.split("@")[0].strip()
-  # print(f"  INST: {str.encode(inst_line)}")
-  sep_inst = inst_line.split(" ")
-  if len(sep_inst) < 3:
-    print(f" PARSE ERROR: {inst_line}")
+branch_instruction_list = ["b", "bl", "blx", "bx"]
+multiply_instruction_list = ["mla", "mul", "smlal", "smull", "umlal", "umull"]
+memory_instruction_list = ["ldr", "ldrb", "ldrbt", "ldrh", "ldrsb", "ldrsh", "ldrt", 
+                           "str", "strb", "strbt", "strh", "strt"]
 
-  # Data Processing
-  # cond[31:28]
-  # 27: 0, 26: 0, 25: (1: imm, 0: not imm)
-  # opcode[24:21]
-  # s[20]
-  # rn[19:16]
-  # rd[15:12]
-  # operand2[11:0]
-  # TODO: Add for Multiple
-  opcode = sep_inst[0].casefold()
-  ops = [ ops.strip(", ") for ops in sep_inst[1:] ]
-  print(f"\t {opcode}\t{ops}")
-  is_imm = False
+class ShifterOperandObj:
+  def __init__(self, shifter_operand: str):
+    self.is_only_imm = False
+    self.is_only_reg = False
+    self.is_rrx = False
+    self.rm = 0
+    self.imm = 0
+    self.shifter = None
+    self.shift_imm = 0
+    self.rs = 0
 
-  # opcode
-  if opcode[0:3] not in opcode_map.keys():
-    print(f"OPCODE ERROR: {opcode}")
+    operands = [ opr.strip() for opr in shifter_operand.split(",") ]
 
-  # 1. move instruction (mov rd, <operand2>)
-  if opcode == "mov":
-    rd = ops[0]
-    operand2 = ops[1:]
-    # rd (r0 -> 0x0, r1 -> 0x1, r2 -> 0x2)
-    rd_num = int(rd[1:])
-    # operand2
-    # immediate?
-    if operand2[0].startswith("#"):
-      # 32bit-imm
-      imm = int(operand2[0][1:])
-      is_imm = True
-      print(f"imm: {imm}")
-    else:
-      rm = operand2[0]
-      if len(operand2) == 1:
-        rm_num = int(rm[1:])
-        print(f"only register: {rm_num}")
-      elif len(operand2) == 2:
-        # rm, rrx
-        if operand2[1] != "rrx":
-          print("not rrx!")
-      elif len(operand2) == 3:
-        # rm, {shift} imm
-        if operand2[2].startswith("#"):
-          shift_imm = int(operand2[2][1:])
-        # rm, {shift} rs
-        else:
-          rs_num = int(operand2[2][1:])
+    if len(operands) == 1:
+      if operands[0].startswith("#"):
+        self.is_only_imm = True
+        self.imm = int(operands[0][1:])
       else:
-        print("ERROR")
-    # make a bit stream
-    encoded = bits(cond_map["al"], 28) | bits(0, 27) | bits(0, 26)
-    if is_imm:
-      encoded |= bits(1, 25)
+        self.is_only_reg = True
+        self.rm = int(operands[0][1:])
     else:
-      encoded |= bits(0, 25)
-    encoded |= bits(opcode_map["mov"],21) | bits(0, 20) | bits(0, 16) | bits(rd_num, 12)
-    # TODO: Rotate imm?
-    encoded |= bits(0, 9) | bits(imm, 0)
-    print(f"encoded binary: {bin(encoded)} [{hex(encoded)}]")
+      self.rm = int(operands[0][1:])
+      shifter_split = operands[1].split(" ")
+      if len(shifter_split) == 1:
+        self.is_rrx = True
+      else:
+        self.shifter = shifter_split[0]
+        if shifter_split[1].startswith("#"):
+          self.shift_imm = int(shifter_split[1][1:])
+        else:
+          self.rs = int(shifter_split[1][1:])
 
-  # 2. add instruction (add rd, rn, <operand2>)
-  elif opcode == "add":
+  def dump(self):
+    print(f"imm\t[#{self.imm}]\t\trm\t[R{self.rm}]\nshifter\t[{self.shifter}]\t\timm\t[#{self.shift_imm}]\trs\t[R{self.rs}]")
+
+
+class InstructionObj:
+  @classmethod
+  def create(cls, line_number: int, line_string: str):
+    print(f"CREATE [line:{line_number:>3d}]\t{line_string:<80}")
+    line_elem = line_string.split(" ")
+    mnemonic = line_elem[0].casefold()
+
+    if mnemonic in data_opcode_map.keys():
+      num = int(line_elem[1].rstrip(",")[1:])
+      if mnemonic in data_processing_type1_list:
+        return cls(line_string, mnemonic, rd=num, shifter_operand=line_elem[2])
+      elif mnemonic in data_processing_type2_list:
+        return cls(line_string, mnemonic, rn=num, shifter_operand=line_elem[2])
+      elif mnemonic in data_processing_type3_list:
+        num2 = int(line_elem[2].rstrip(",")[1:])
+        return cls(line_string, mnemonic, rd=num, rn=num2, shifter_operand=line_elem[3])
+      else:
+        print(f"ERROR 1 : {mnemonic}")
+        return None
+    elif mnemonic in branch_instruction_list:
+      if len(line_elem) != 2:
+        print(f"ERROR 2 : {mnemonic}")
+        return None
+      return cls(line_string, mnemonic, label=line_elem[1])
+    elif mnemonic in multiply_instruction_list:
+      return cls(line_string, mnemonic)
+    elif mnemonic in memory_instruction_list:
+      return None
+    else:
+      return None
+      
+  def __init__(self, line_string: str, name: str, label: str=None, rd: str=None, rn: str=None, shifter_operand: str=None):
+    self.name = name
+    self.line_string = line_string
+    self.has_condition = False
+    self.label = label
+    self.rd = rd
+    self.rn = rn
+    self.shifter_obj = None if shifter_operand == None else ShifterOperandObj(shifter_operand)
+
+  def is_branch(self):
+    return self.label != None
+  
+  def is_memory(self):
     pass
-  else:
-    print(f"NOT SUPPORT ERROR: {inst_line}")
+
+  def is_multiply(self):
+    pass
+
+  def is_type1_dataprocessing(self):
+    return self.rd != None and self.shifter_obj != None
+
+  def is_type2_dataprocessing(self):
+    return self.rn != None and self.shifter_obj != None
+
+  def is_type3_dataprocessing(self):
+    return self.rd != None and self.rn != None and self.shifter_obj != None
+  
+  def dump(self):
+    print(f"{self.line_string:^80}")
+    rd = f"R{self.rd}" if self.rd is not None else "-"
+    rn = f"R{self.rn}" if self.rn is not None else "-"
+    label = f"{self.label}" if self.label is not None else "-"
+    print(f"rd\t[{rd}]\t\trn\t[{rn}]\tlabel\t[{label}]")
+    if self.shifter_obj != None:
+      self.shifter_obj.dump()
+    print("="*80)
+
+class Parser:
+  def __init__(self, filepath=None):
+    self._file = open(filepath, "r")
+    self._not_support_count = 0
+
+  def parse(self) -> list[InstructionObj]:
+    objs = []
+    for i, line in enumerate(self._file.readlines()):
+      ret, line = Parser.cleaner(line)
+      if ret: continue
+
+      line = Parser.remove_line_comment(line)
+      if Parser.is_directives(line):
+        pass
+      elif Parser.is_label(line):
+        print(f"not support yet:\t{line}")
+        self._not_support_count += 1
+      else:
+        objs.append(InstructionObj.create(i+1, line))
+
+    self._file.close()
+
+    return objs
+
+  @staticmethod
+  def cleaner(line: str):
+    return ((line.isspace() or len(line) == 0 or line.startswith(("@",";"))), line.rstrip())
+  
+  @staticmethod
+  def is_directives(line: str):
+    return line.startswith(".")
+  
+  @staticmethod
+  def is_label(line: str):
+    return line.endswith(":")
+  
+  @staticmethod
+  def remove_line_comment(line: str):
+    return line.split("@")[0].strip()
+
+class Encoder:
+  def __init__(self):
+    self.encoding_bits = 0x00000000
+
+  def encode(self, objs: list[InstructionObj]):
+    for obj in objs:
+      # Now, use only always condition
+      self.set_condition_flag_bit("al")
+      if obj.is_type1_dataprocessing() or obj.is_type2_dataprocessing() or obj.is_type3_dataprocessing():
+        self.data_processing_encoding(obj)
+
+  def data_processing_encoding(self, obj: InstructionObj):
+    self.set_bit(0, 27)
+    self.set_bit(0, 26)
+    if obj.is_type1_dataprocessing:
+      self.set_bit(data_opcode_map[obj.name], 21)
+      self.set_bit(0, 20) # s bit, now 0
+      self.set_bit(0, 16)
+      # TODO next!
+
+  def branch_encoding(self):
+    pass
+
+  def memory_encoding(self):
+    pass
+
+  def multiply_encoding(self):
+    pass
+
+  def set_condition_flag_bit(self, name: str):
+    self.encoding_bits |= condition_code_map[name] << 28
+
+  def set_bit(self, val, pos):
+    self.encoding_bits |= val << pos
 
 def check():
   with open("sample/from-chatgpt/objdir/data_processing.o", "rb") as of:
@@ -170,18 +265,11 @@ if __name__ == "__main__":
   if enable_check == "1":
     check()
 
-  with open("sample/from-chatgpt/data_processing.s", "r") as f:
-    for line in f.readlines():
-      line = line.rstrip()
-      if line.isspace() or len(line) == 0:
-        continue
+  p = Parser("sample/from-chatgpt/data_processing.s")
+  objs = p.parse()
 
-      # instructions have indentation.
-      if len(line) - len(line.lstrip()) > 0:
-        process_instruction()
-      elif line.startswith("."):
-        print(f"{'DIRECTIVES':<40}{line:>20}")
-      elif line.startswith("@"):
-        print(f"{'LINE COMMENTS':<40}{line:>20}")
-      else:
-        print(f"{'NOT SUPPORT LINE':<40}{line:>20}")
+  for o in objs:
+    o.dump()
+
+  # e = Encoder()
+  # e.encode
