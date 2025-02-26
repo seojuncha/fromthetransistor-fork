94ae2c0840cd5b66817b2675a843db97c30a6f25
Cha Seojun <sjcha36@gmail.com>
2025-02-26 15:33:42 +0900

    fix indexing error

--- mainproject/c-compiler/scanner.lisp
+++ mainproject/c-compiler/scanner.lisp
@@ -2,12 +2,12 @@
 
 (in-package :c-compiler)
 
-(defconstant +token-1-open-paran+ #\()
-(defconstant +token-1-close-paran+ #\))
-(defconstant +token-1-open-curly-bracket+ #\{)
-(defconstant +token-1-close-curly-bracket+ #\})
-(defconstant +token-1-equal+ #\=)
-(defconstant +token-1-semicolon+ #\;)
+(defconstant +token-open-paran+ #\()
+(defconstant +token-close-paran+ #\))
+(defconstant +token-open-curly-bracket+ #\{)
+(defconstant +token-close-curly-bracket+ #\})
+(defconstant +token-equal+ #\=)
+(defconstant +token-semicolon+ #\;)
 (defconstant +token-newline+ #\newline)
 (defconstant +token-space+ #\space)
 
@@ -27,6 +27,43 @@
 
 (defparameter *tokens* '())
 
+(defun scanning (source)
+  (format t ">>> start scanning.... ~d characters.~%" (length source))
+  (loop with line = 1 and index = 0
+        while (< index (length source)) do
+    (let ((start index)
+          (offset 0)
+          (character nil))
+      (setq character (char source index))
+      (format t "[line:~d][index:~d] ~a~%" line index character)
+
+      (cond 
+        ((char= character +token-open-paran+)
+          (push (make-token :token-type :open-paran :line line) *tokens*) (incf index))
+        ((char= character +token-close-paran+)
+          (push (make-token :token-type :close-paran :line line) *tokens*) (incf index))
+        ((char= character +token-open-curly-bracket+)
+          (push (make-token :token-type :open-curly-bracket :line line) *tokens*) (incf index))
+        ((char= character +token-close-curly-bracket+)
+          (push (make-token :token-type :close-curly-bracket :line line) *tokens*) (incf index))
+        ((char= character +token-semicolon+)
+          (push (make-token :token-type :semicolon :line line) *tokens*) (incf index))
+
+        ((char= character +token-space+)
+          (incf index))
+        ((char= character +token-newline+)
+          (incf line) (incf index))
+        (t (cond
+              ((digit-char-p character) 
+                (format t "numeric~%") (incf index))
+              ((alpha-char-p character)
+                (format t "alphabet~%")
+                (incf index (identifier source start offset)))
+              (t (format t "unkown: ~s~%" character) (incf index))))
+          )))
+  (push (make-token :token-type :eof) *tokens*)
+  *tokens*)
+
 (defun identifier (source start offset)
   (loop while (or (digit-char-p (char source (+ start offset))) (alpha-char-p (char source (+ start offset)))) do
                           (incf offset))
@@ -34,35 +71,6 @@
 
   (let ((substring nil))
     (setq substring (subseq source start (+ start offset)))
-    (format t "substring: ~a~%" substring)
+    (format t "substring: ~s~%" substring)
     ; compare wheter it is keyword.
   ) offset)
-
-
-(defun scanning (source)
-  (format t ">>> start scanning.... ~d characters.~%" (length source))
-  (loop with line = 1 and index = 0
-        while (< index (length source)) do
-    (let ((start index)
-          (offset 0)
-          (chracter nil))
-      (setq chracter (char source index))
-      (incf index)
-      (format t "[line:~d][index:~d] ~a~%" line index chracter)
-
-      (cond 
-        ((char= chracter +token-1-open-paran+)
-          (push (make-token :token-type :open-paran :line line) *tokens*))
-        ((char= chracter +token-1-close-paran+)
-          (push (make-token :token-type :close-paran :line line) *tokens*))
-        ((char= chracter +token-newline+)
-          (incf line))
-        (t (cond
-              ((digit-char-p chracter) 
-                (format t "numeric~%"))
-              ((alpha-char-p chracter)
-                (format t "alphabet~%")
-                (incf index (identifier source start offset)))))
-          )))
-  (push (make-token :token-type :eof) *tokens*)
-  *tokens*)
